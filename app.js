6cd6d54596ad415c485c98425bdbd918f6ee4dcd
irynabolyukh <iryna.boliukh@ukma.edu.ua>
2021-03-29 23:12:19 +0300

    simple auth routes, database connected

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan'); 
 const dotenv = require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,7 +17,7 @@ app.use(morgan('dev'));
 app.set('view engine', 'ejs');
 
 // database connection
-const dbURI = 'mongodb+srv://node-mailer:node-mailer@node-rest-mailer.p4zyl.mongodb.net/node-mailer?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://brain-training:' + process.env.MONGO_ATLAS_PW + '@braintraining.p4zyl.mongodb.net/' + process.env.MONGO_ATLAS_NM;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(PORT))
   .catch((err) => console.log(err));
@@ -24,3 +25,5 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/games', (req, res) => res.render('games'));
+app.use(authRoutes);
+
